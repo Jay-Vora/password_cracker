e94d5baefedd5acdd9140f7fd879c52963091edf
vora1900 <vora1900@mylaurier.ca>
2024-05-09 21:07:28 -0400

    compression func. and readability added

--- md5Algo.py
+++ md5Algo.py
@@ -1,7 +1,34 @@
 #imports
-from ctypes import c_uint32
 
 
+#CONSTANTS
+# Define the K constants for the four rounds
+K = [
+    0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+    0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+    0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+    0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+
+    0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+    0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+    0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+    0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+
+    0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+    0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+    0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
+    0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+
+    0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+    0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+    0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+    0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
+]
+# Initialize the four buffers A, B, C, D with their respective initial values
+A = 0x67452301 
+B = 0xefcdab89 
+C = 0x98badcfe
+D = 0x10325476 
 
 #to represent the msg in binary format
 def binaryRepresentation(inputMsg) :
@@ -63,17 +90,7 @@ def padding(inputBits, inputMsg) :
 
 inputmsg = "nghgwegfwjgngsghdvsbvdghdhfghdhvjdjhvbnhfdbhdbndhjvbbdfhchdsbvhdhcvbsvbvghsgvshvhdbvbdnhvbndbvb"
 msgBits = binaryRepresentation(inputmsg)
-# print(binaryRepresentation(inputmsg))
-# x = inputmsg + '1'
-# y, z = multipleOf512(x)
-# print(y, z)
 
-#print(padding(msgBits, inputmsg))
-# print(len(padding(msgBits,inputmsg)))
-
-
-# def mainCompressionFunction(F,G,H,I):
-#     #PROBABLY HERE THE FUNCTIONS WILL COME
 
 #now the task is to divide the msg into equal blocks of 512 bits
 def split_into_blocks(paddedMessage):
@@ -87,32 +104,13 @@ def split_into_blocks(paddedMessage):
 
     return blocks
 
-x = split_into_blocks(padding(msgBits, inputmsg))
-y = x[0]
-#print(split_into_blocks(padding(msgBits, inputmsg)))
-
-# var1 = 0x00000000
-# var2 = 0x00000000
-# var3 = 0x00000000
-# var4 = 0x00000000
-
-# var1 = c_uint32(0)
-# var2 = c_uint32(0)
-# var3 = c_uint32(0)
-# var4 = c_uint32(0)
 
 
-# Initialize the four buffers A, B, C, D with their respective initial values
-A = 0x67452301 
-B = 0xefcdab89 
-C = 0x98badcfe
-D = 0x10325476 
 
-# aBitLength = A.bit_length
-# print(aBitLength)
 
-#message schedul process breaks 512 bits block further down into 16 32 bits subblocks
-def mesageSchedule(inputBlock):
+
+#message schedul process breaks 512 bits block further down into sixteen 32 bits subblocks
+def messageSchedule(inputBlock):
     blockSize = 512
     subBlockSize = 32
     limit = int(blockSize / subBlockSize)
@@ -121,55 +119,92 @@ def mesageSchedule(inputBlock):
         subblocks.append(inputBlock[i * subBlockSize : (i + 1) * subBlockSize])
     
     return subblocks 
-# y =int(512 / 32)
-# print(y)
-print(mesageSchedule(y))
 
 
-# def compressionFunction(subblocks):
-#     for subblock in subblocks:
-#         #for each subblock apply 64 rounds of operations
 
+def compressionFunction(blocks, A, B, C, D):
+    # Define the four auxiliary functions for the rounds
+    def firstRoundFunction(valueB, valueC, valueD):
+        # Implement first round function
+        firstResult = valueB & valueC
+        secondResult = (( ~valueB) & valueD)
+        finalResult = firstResult | secondResult
+        finalResult = hex(finalResult)
+        final = hex_to_binary_32(finalResult)
 
-def firstRoundFunction(valueB, valueC, valueD):
-    #code here
+        return final
 
-    firstResult = valueB & valueC
-    secondResult = (( ~valueB) & valueD)
-    finalResult = firstResult | secondResult
-    finalResult = hex(finalResult)
-    final = hex_to_binary_32(finalResult)
+    def secondRoundFunction(valueB, valueC, valueD):
+        # Implement second round function
+        firstResult = valueB & valueC
+        secondResult = (valueC & (~valueD))
+        finalResult = firstResult | secondResult
+        finalResult = hex(finalResult)
+        final = hex_to_binary_32(finalResult)
 
-    return final
+        return final
 
-print(firstRoundFunction(B,C,D))    
+    def thirdRoundFunction(valueB, valueC, valueD):
+        # Implement third round function
+        firstResult = valueB ^ valueC
+        finalResult = firstResult ^ valueD
+        finalResult = hex(finalResult)
+        final = hex_to_binary_32(finalResult)
 
-def secondRoundFuction(valueB, valueC, valueD):
-    #code here
-    firstResult = valueB & valueC
-    secondResult = (valueC & (~valueD))
-    finalResult = firstResult | secondResult
-    finalResult = hex(finalResult)
-    final = hex_to_binary_32(finalResult)
+        return final
+    def fourthRoundFunction(valueB, valueC, valueD):
+        # Implement fourth round function
+        firstResult = (valueB | (~ valueD))
+        finalResult = valueC ^ firstResult
+        finalResult = hex(finalResult)
+        final = hex_to_binary_32(finalResult)
 
-    return final
+        return final
 
+    # Loop through each block
+    for block in blocks:
+        # Break the block into 32-bit subblocks
+        subblocks = messageSchedule(block)
 
-def thirdRoundFunction(valueB, valueC, valueD):
-    #code here
-    firstResult = valueB ^ valueC
-    finalResult = firstResult ^ valueD
-    finalResult = hex(finalResult)
-    final = hex_to_binary_32(finalResult)
+        # Initialize the four working variables
+        AA = A
+        BB = B
+        CC = C
+        DD = D
 
-    return final
+        # Main round loop
+        for i in range(64):
+            if 0 <= i <= 15:
+                # Perform operations for the first round
+                F = firstRoundFunction(BB, CC, DD)
+                g = i
+            elif 16 <= i <= 31:
+                # Perform operations for the second round
+                F = secondRoundFunction(BB, CC, DD)
+                g = (5 * i + 1) % 16
+            elif 32 <= i <= 47:
+                # Perform operations for the third round
+                F = thirdRoundFunction(BB, CC, DD)
+                g = (3 * i + 5) % 16
+            elif 48 <= i <= 63:
+                # Perform operations for the fourth round
+                F = fourthRoundFunction(BB, CC, DD)
+                g = (7 * i) % 16
 
-def fourthRoundFunction(valueB, valueC, valueD):
-    #code here
-    firstResult = (valueB | (~ valueD))
-    finalResult = valueC ^ firstResult
-    finalResult = hex(finalResult)
-    final = hex_to_binary_32(finalResult)
+            # Temporarily save DD
+            temp = DD
+            # Update DD
+            DD = CC
+            CC = BB
+            BB = BB + leftrotate((AA + F + K[i] + subblocks[g]), s[i])
+            AA = temp
 
-    return final
+        # Update the values of A, B, C, and D with the results of this block
+        A = A + AA
+        B = B + BB
+        C = C + CC
+        D = D + DD
+
+    # Return the hash value
+    return A, B, C, D
 
